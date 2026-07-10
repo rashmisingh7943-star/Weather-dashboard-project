140c2aa7044751610e4183744a49f98f7ae441da
Rashmi Singh <Rashmi Singh>
2026-07-10 16:07:05 +0530

    Display weather condition

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ const data = await response.json();
 output.innerHTML = `
     <h3>${data.name}</h3>
     <p>Temperature: ${data.main.temp} °C</p>
+        <p>Condition: ${data.weather[0].main}</p>
+
 `;
 
 } catch (error) {
